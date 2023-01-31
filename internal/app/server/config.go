a0d7d9f237d25bb53f73562d5006a385634988a5
EwvwGeN <doj54567467z@gmail.com>
2023-01-31 18:35:43 +0300

    changed type of port to string

--- internal/app/server/config.go
+++ internal/app/server/config.go
@@ -2,14 +2,14 @@ package server
 
 type Config struct {
 	Host   string `yaml:"host"`
-	Port   int    `yaml:"port"`
+	Port   string `yaml:"port"`
 	DBname string `yaml:"dbname"`
 }
 
 func NewConfig() *Config {
 	return &Config{
 		Host:   "localhost",
-		Port:   6534,
+		Port:   "6534",
 		DBname: "testdb",
 	}
 }
