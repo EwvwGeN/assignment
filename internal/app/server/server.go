068c9a9fe99329885f28c3835a603c94348ffee1
EwvwGeN <doj54567467z@gmail.com>
2023-02-11 17:56:15 +0300

    api for simple doc(without view of child)

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,21 +1,29 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
 
 	"github.com/EwvwGeN/assignment/internal/models"
+	"github.com/gin-gonic/gin"
 	"github.com/restream/reindexer"
 )
 
 type Server struct {
+	router *gin.Engine
 	config *Config
 	db     *reindexer.Reindexer
 }
 
 func NewServer(config *Config) *Server {
 	DbConn := reindexer.NewReindex(
-		fmt.Sprintf("cproto://%s:%s/%s", config.Host, config.Port, config.DBname), reindexer.WithCreateDBIfMissing())
+		fmt.Sprintf("cproto://%s:%s/%s", config.DbHost, config.DbPort, config.DBname), reindexer.WithCreateDBIfMissing())
 	return &Server{
+		router: gin.Default(),
 		config: config,
 		db:     DbConn,
 	}
@@ -31,5 +39,91 @@ func (server *Server) Start() {
 	}
 
 	server.prepareCollections()
+	server.configureRouter()
 	fmt.Println("Server started")
+}
+
+func (server *Server) configureRouter() {
+	simpleDocGroupe := server.router.Group("/docs")
+	{
+		simpleDocGroupe.GET("/all", server.getAllDocs)
+		simpleDocGroupe.GET("/id=:id", server.getDocById)
+		simpleDocGroupe.POST("", server.createDoc)
+		simpleDocGroupe.PUT("", server.updateDoc)
+		simpleDocGroupe.DELETE("/id=:id", server.deleteDoc)
+	}
+	server.router.Run(fmt.Sprintf("%s:%s", server.config.ApiHost, server.config.APiPort))
+}
+
+func (server *Server) createDoc(ctx *gin.Context) {
+	var newDocument models.Document
+	ctx.BindJSON(&newDocument)
+	fmt.Println(newDocument)
+	server.db.Upsert(server.config.CollectionName, &newDocument, "id=serial()")
+	ctx.IndentedJSON(http.StatusOK, newDocument)
+}
+
+func (server *Server) getAllDocs(ctx *gin.Context) {
+	query := server.db.Query(server.config.CollectionName)
+	iterator := query.Exec()
+	defer iterator.Close()
+	for iterator.Next() {
+		elem := iterator.Object().(*models.Document)
+		ctx.IndentedJSON(http.StatusOK, elem)
+		fmt.Println(*elem)
+	}
+}
+
+func (server *Server) getDocById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	doc, found := server.findDoc(id)
+	if !found {
+		ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "document doesnt exist"})
+		return
+	}
+	ctx.IndentedJSON(http.StatusOK, doc)
+}
+
+func (server *Server) updateDoc(ctx *gin.Context) {
+	var document models.Document
+	var jsonData map[string]interface{}
+	data, _ := ioutil.ReadAll(ctx.Request.Body)
+	json.Unmarshal(data, &document)
+	json.Unmarshal(data, &jsonData)
+	_, found := server.findDoc(document.Id)
+	if !found {
+		ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "document doesnt exist"})
+		return
+	}
+	query := server.db.Query(server.config.CollectionName).Where("id", reindexer.EQ, document.Id)
+	types := reflect.TypeOf(document)
+	for key, item := range jsonData {
+		if field, exist := types.FieldByName(key); exist {
+			query.Set(field.Name, item)
+		}
+	}
+	query.Update()
+
+	ctx.IndentedJSON(http.StatusOK, gin.H{"message": "ok"})
+}
+
+func getReindFieldName(field reflect.StructField) string {
+	return strings.Split(field.Tag.Get("reindex"), ",")[0]
+}
+
+func (server *Server) deleteDoc(ctx *gin.Context) {
+	id := ctx.Param("id")
+	doc, found := server.findDoc(id)
+	if !found {
+		ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "document doesnt exist"})
+		return
+	}
+	server.db.Delete(server.config.CollectionName, doc)
+	ctx.IndentedJSON(http.StatusOK, doc)
+}
+
+func (server *Server) findDoc(id interface{}) (interface{}, bool) {
+	query := server.db.Query(server.config.CollectionName).Where("id", reindexer.EQ, id)
+	doc, found := query.Get()
+	return doc, found
 }
--- internal/models/document.go
+++ internal/models/document.go
@@ -1,8 +1,8 @@
 package models
 
 type Document struct {
-	Id        int64   `reindex:"id,,pk"`
-	ParentId  int64   `reindex:"parent_id"`
-	Body      string  `reindex:"body"`
-	ChildList []int64 `reindex:"child_list"`
+	Id        int64   `reindex:"id,,pk" json:"Id"`
+	ParentId  int64   `reindex:"parent_id,,sparse" json:"ParentId"`
+	Body      string  `reindex:"body" json:"Body"`
+	ChildList []int64 `reindex:"child_list,,sparse" json:"ChildList"`
 }
