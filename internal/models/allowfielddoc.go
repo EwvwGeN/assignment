8e1ffe2d1ea563fc53c31ceadc74846e241f9760
EwvwGeN <doj54567467z@gmail.com>
2023-03-08 11:02:29 +0300

    edited document structure

--- internal/models/allowfielddoc.go
+++ internal/models/allowfielddoc.go
@@ -1,7 +1,6 @@
 package models
 
 type AllowedField struct {
-	Id        int64   `reindex:"id,,pk" json:"Id"`
 	Body      string  `reindex:"body" json:"Body"`
 	ChildList []int64 `reindex:"child_list,,sparse" json:"ChildList"`
 }
--- internal/models/document.go
+++ internal/models/document.go
@@ -2,7 +2,8 @@ package models
 
 type Document struct {
 	Id        int64   `reindex:"id,,pk" json:"Id"`
-	ParentId  int64   `reindex:"parent_id" json:"ParentId"`
+	ParentId  int64   `reindex:"parent_id,,sparse" json:"ParentId"`
+	Depth     int     `reindex:"depth" json:"Depth"`
 	Body      string  `reindex:"body" json:"Body"`
 	ChildList []int64 `reindex:"child_list,,sparse" json:"ChildList"`
 }
