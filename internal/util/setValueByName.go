9f7cfe57224cf57dfea90d7d720cce5039238ad7
EwvwGeN <doj54567467z@gmail.com>
2023-05-04 00:25:42 +0300

    fixed bag with array of int64

--- internal/util/setValueByName.go
+++ internal/util/setValueByName.go
@@ -1,9 +1,16 @@
 package util
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func SetValueByName(v interface{}, field string, newval interface{}) {
 	r := reflect.ValueOf(v).Elem().FieldByName(field)
+	if reflect.TypeOf(newval).Kind() == reflect.Slice {
+		intArray := ArrToInt64(newval.([]interface{}))
+		r.Set(reflect.ValueOf(intArray))
+		return
+	}
 	r.Set(reflect.ValueOf(newval))
 
 }
